6ceb46555037cd86c13d97e5c9dfaeae93760905
HyeokJin-Choi <126738953+HyeokJin-Choi@users.noreply.github.com>
2025-01-05 00:38:21 +0900

    Update infoUniv.py

--- infoUniv.py
+++ infoUniv.py
@@ -1,14 +1,16 @@
-## School 컬럼 추가 school_address
-
 import pandas as pd
 import pymysql
 
 # 1. CSV 파일 읽기
 file_path = '전국대학및전문대학정보표준데이터.csv'
-data = pd.read_csv(file_path, usecols=['학교명', '시도명', '소재지도로명주소'])
+data = pd.read_csv(file_path, usecols=['학교명'])
 data.fillna('', inplace=True)  # NaN 값 처리
 
-# 2. MySQL 연결 설정
+# 2. 대학명 중복 제거
+data['학교명'] = data['학교명'].str.split().str[0]  # 첫 번째 단어 기준으로 추출
+data = data.drop_duplicates(subset=['학교명'])  # 중복 제거
+
+# 3. MySQL 연결 설정
 connection = pymysql.connect(
     host='0.0.0.0',  # MySQL 서버 주소
     user='checkjin_2023874',  # MySQL 사용자 이름
@@ -17,14 +19,14 @@ connection = pymysql.connect(
     charset='utf8mb4'
 )
 
-# 3. 데이터 삽입
+# 4. 데이터 삽입
 try:
     with connection.cursor() as cursor:
         # INSERT 쿼리 준비
-        insert_query = "INSERT INTO School (school_name, school_local, school_address) VALUES (%s, %s, %s)"
+        insert_query = "INSERT INTO School (school_name) VALUES (%s)"
         # 데이터 삽입
         for _, row in data.iterrows():
-            cursor.execute(insert_query, (row['학교명'], row['시도명'], row['소재지도로명주소']))
+            cursor.execute(insert_query, (row['학교명'],))
     connection.commit()
     print("데이터 삽입 완료!")
 finally:
